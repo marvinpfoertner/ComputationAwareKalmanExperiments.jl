8aae1397f526cfe9e19ebc6b107acf31b03aa0c9
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-10-26 21:43:12 +0200

    EnKF predict

---
+++ src/enkf/predict.jl
@@ -0,0 +1,14 @@
+function predict_sample_pointwise(
+    gmc::ComputationAwareKalman.AbstractGaussMarkovChain,
+    k::Integer,
+    uₖ₋₁::Ensemble,
+    rng::Random.AbstractRNG,
+)
+    Sₖ₋₁ = members(uₖ₋₁)
+    Sₖ = hcat([rand(rng, gmc, k, Sₖ₋₁[:, i]) for i in axes(Sₖ₋₁, 2)]...)
+
+    mₖ = sum(Sₖ, dims = 2) / size(Sₖ, 2)
+    Zₖ = (Sₖ .- mₖ) / sqrt(size(Sₖ, 2) - 1)
+
+    return EnsembleGaussian(mₖ, Zₖ)
+end
