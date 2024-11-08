70771e1fe4e8226c51ad7fe5e17c38cfba72f56c
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-11-08 14:31:07 +0100

    Annotate EnKF code

--- src/enkf/update.jl
+++ src/enkf/update.jl
@@ -8,16 +8,19 @@ function update_enkf(
     Λ::AbstractMatrix,
     rng::Random.AbstractRNG,
 )
-    Eᶠ = members(u⁻)  # forecasting ensemble
-    N = size(Eᶠ, 2)  # number of ensemble members
-    Xᶠ = u⁻.Z  # forecasting ensemble-anomaly matrix
+    # The implementation is based on section 4.1 the paper
+    # Carrassi, A., Bocquet, M., Bertino, L., and Evensen, G. Data assimilation in the geosciences: An overview of methods, issues, and perspectives. WIREs Clim Change. 2018.
 
-    Yₒ = y .+ sqrt(Λ) * randn(rng, eltype(Λ), (length(y), N))  # perturbed observations
+    Eᶠ = members(u⁻)  # forecasting ensemble, Eq. 32
+    N = size(Eᶠ, 2)  # number of ensemble members, Eq. 32
+    Xᶠ = u⁻.Z  # forecasting ensemble-anomaly matrix, Eq. 34
 
-    Y = H * Xᶠ  # transformed ensemble-anomaly matrix
-    C = SymWoodbury(Λ, Y, I(N))  # innovation matrix
+    Yₒ = y .+ sqrt(Λ) * randn(rng, eltype(Λ), (length(y), N))  # perturbed observations, Eqs. 37 & 38
 
-    Eᵃ = Eᶠ + Xᶠ * Y' * (C \ (Yₒ - H * Eᶠ))  # analysis ensemble
+    Y = H * Xᶠ  # transformed ensemble-anomaly matrix, Eq. 47
+    C = SymWoodbury(Λ, Y, I(N))  # innovation matrix, Eq. 48 (and text below)
+
+    Eᵃ = Eᶠ + Xᶠ * Y' * (C \ (Yₒ - H * Eᶠ))  # analysis ensemble, Eq. 44
 
     return EnsembleGaussian(Eᵃ)
 end
