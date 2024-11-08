7022b06043c9121e4d53e8e17bd59a4747218787
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-11-08 16:14:41 +0100

    Implement ETKF

--- src/enkf/update.jl
+++ src/enkf/update.jl
@@ -8,8 +8,8 @@ function update_enkf(
     Λ::AbstractMatrix,
     rng::Random.AbstractRNG,
 )
-    # The implementation is based on section 4.1 the paper
-    # Carrassi, A., Bocquet, M., Bertino, L., and Evensen, G. Data assimilation in the geosciences: An overview of methods, issues, and perspectives. WIREs Clim Change. 2018.
+    # This implementation is based on section 4.1 of the paper
+    # Carrassi, A., Bocquet, M., Bertino, L., and Evensen, G. "Data assimilation in the geosciences: An overview of methods, issues, and perspectives." WIREs Clim Change. 2018.
 
     Eᶠ = members(u⁻)  # forecasting ensemble, Eq. 32
     N = size(Eᶠ, 2)  # number of ensemble members, Eq. 32
@@ -20,7 +20,38 @@ function update_enkf(
     Y = H * Xᶠ  # transformed ensemble-anomaly matrix, Eq. 47
     C = SymWoodbury(Λ, Y, I(N))  # innovation matrix, Eq. 48 (and text below)
 
-    Eᵃ = Eᶠ + Xᶠ * Y' * (C \ (Yₒ - H * Eᶠ))  # analysis ensemble, Eq. 44
+    Eᵃ = Eᶠ + Xᶠ * (Y' * (C \ (Yₒ - H * Eᶠ)))  # analysis ensemble, Eq. 44
 
     return EnsembleGaussian(Eᵃ)
+end
+
+
+function update_etkf(
+    u⁻::EnsembleGaussian,
+    y::AbstractVector,
+    H::AbstractMatrix,
+    Λ::AbstractMatrix,
+)
+    # This implementation is based on section 3.a of the paper
+    # Tippett, M. K., Anderson, J. L., Bishop, C. H., Hamill, T. M., Whitaker, J. S. "Ensemble square root filters." Mon. Wea. Rev., 131, 1485-1490, 2003.
+
+    Zᶠ = u⁻.Z  # square root of forecast covariance
+    HZᶠ = H * Zᶠ  # square root of transformed forecast covariance
+
+    γs, C = eigen!(hermitianpart!(HZᶠ' * (Λ \ HZᶠ)))
+    Zᵃ = Zᶠ * C * Diagonal(sqrt.(γs .+ 1.0))  # square root of analysis covariance, Eq. 16
+
+    return EnsembleGaussian(update_mean(u⁻, y, H, Λ, HZᶠ), Zᵃ)
+end
+
+function update_mean(
+    u⁻::EnsembleGaussian,
+    y::AbstractVector,
+    H::AbstractMatrix,
+    Λ::AbstractMatrix,
+    HZ::AbstractMatrix = H * u⁻.Z,
+)
+    S = SymWoodbury(Λ, HZ, I(size(u⁻.Z, 2)))  # innovation covariance
+
+    return u⁻.m + u⁻.Z * (HZ' * (S \ (y - H * u⁻.m)))
 end
