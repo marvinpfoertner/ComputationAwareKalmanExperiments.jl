668f3abc6f4ed63c0b35ad250fa81c42df8617e2
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-11-08 20:45:31 +0100

    ETKF filter loop

--- experiments/baselines/01_filter.jl
+++ experiments/baselines/01_filter.jl
@@ -43,3 +43,35 @@ function filter_enkf(dgmp, H, Λ, ys_train, rng, rank)
 
     return us[2:end]
 end
+
+function filter_etkf(dgmp, H, Λ, ys_train, rng, rank)
+    E₀ = hcat([rand(rng, dgmp, 0) for _ = 1:rank]...)
+    u₀ = ComputationAwareKalmanExperiments.EnsembleKalmanFilter.EnsembleGaussian(E₀)
+
+    us = [u₀]
+
+    for k in axes(ys_train, 1)
+        u⁻ₖ =
+            ComputationAwareKalmanExperiments.EnsembleKalmanFilter.predict_sample_pointwise(
+                dgmp,
+                k - 1,
+                us[end],
+                rng,
+            )
+
+        if ismissing(ys_train[k])
+            uₖ = u⁻ₖ
+        else
+            uₖ = ComputationAwareKalmanExperiments.EnsembleKalmanFilter.update_etkf(
+                u⁻ₖ,
+                ys_train[k],
+                H,
+                Λ,
+            )
+        end
+
+        push!(us, uₖ)
+    end
+
+    return us[2:end]
+end
