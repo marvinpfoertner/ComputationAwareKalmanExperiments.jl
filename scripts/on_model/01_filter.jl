cd9e5cbb2abac884caddac66b55640824ce7648a
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-11-13 15:25:26 +0100

    Add CAKF to `on_model` experiment

--- scripts/on_model/01_filter.jl
+++ scripts/on_model/01_filter.jl
@@ -8,3 +8,15 @@ rng = Random.seed!(seed + 1)
 
 ys_train_aug = Vector{Union{eltype(ys_train),Missing}}(missing, length(ts))
 ys_train_aug[ts_train_idcs] = ys_train
+
+function cakf(dgmp, mmod, ys; rank, ts)
+    fcache = ComputationAwareKalman.filter(
+        dgmp,
+        mmod,
+        ys;
+        update_kwargs = (max_iter = rank,),
+        truncate_kwargs = (max_cols = rank,),
+    )
+
+    return [ComputationAwareKalman.interpolate(dgmp, fcache, t) for t in ts]
+end
