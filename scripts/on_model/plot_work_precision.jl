5af8e3456c9134a619f6e2f7be92b19d70930c30
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-11-14 16:44:00 +0100

    Update work precision plot

--- scripts/on_model/plot_work_precision.jl
+++ scripts/on_model/plot_work_precision.jl
@@ -3,6 +3,45 @@ using DrWatson
 
 include("common.jl")
 
+# run_all()
+
+# Collect metrics
+using DataFrames
+
+df = collect_results(datadir("on_model", "results"), black_list = ["uᶠs"])
+
+function collect_deterministic_metrics(algorithm)
+    df_algo =
+        dropmissing(df[df.algorithm.==algorithm, [:rank, :mse, :expected_nll, :wall_time]])
+    return sort!(df_algo, :rank)
+end
+
+function collect_stochastic_metrics(algorithm)
+    df_algo = dropmissing(
+        df[df.algorithm.==algorithm, [:rank, :seed, :mse, :expected_nll, :wall_time]],
+    )
+    df_metric_stats = combine(
+        groupby(df_algo, :rank),
+        :mse => median,
+        :mse => (v -> quantile(v, 0.25)) => :mse_25,
+        :mse => (v -> quantile(v, 0.75)) => :mse_75,
+        :expected_nll => median,
+        :expected_nll => (v -> quantile(v, 0.25)) => :expected_nll_25,
+        :expected_nll => (v -> quantile(v, 0.75)) => :expected_nll_75,
+        :wall_time => median => :wall_time,
+    )
+
+    return sort!(df_metric_stats, :rank)
+end
+
+metrics = Dict(
+    "srkf" => first(dropmissing(df[df.algorithm.=="srkf", [:mse, :expected_nll]])),
+    "enkf" => collect_stochastic_metrics("enkf"),
+    "etkf" => collect_stochastic_metrics("etkf"),
+    "cakf" => collect_deterministic_metrics("cakf"),
+)
+
+# Generate plot
 using CairoMakie
 using TuePlots
 
@@ -19,33 +58,12 @@ T = Theme(
     ncols = 1,
 )
 
-# Assemble metrics
-get_metrics(results) = (mse = results["mse"], expected_nll = results["expected_nll"])
-
-metrics = Dict(
-    "srkf" => get_metrics(results(configs["srkf"])),
-    "enkf" => Dict(
-        rank => get_metrics(results(config)) for (rank, config) in configs["enkf"]
-    ),
-    "etkf" => Dict(
-        rank => get_metrics(results(config)) for (rank, config) in configs["etkf"]
-    ),
-    "cakf" => Dict(
-        rank => get_metrics(results(config)) for (rank, config) in configs["cakf"]
-    ),
-)
-
 with_theme(T) do
     fig = Figure()
 
-    ax_mse = Axis(fig[1, 1], xlabel = "Rank", ylabel = "MSE", xscale = log2, yscale = log10)
-    ax_nll = Axis(
-        fig[2, 1],
-        xlabel = "Rank",
-        ylabel = "Expected NLL",
-        xscale = log2,
-        yscale = log10,
-    )
+    ax_mse =
+        Axis(fig[1, 1], xlabel = "Rank", ylabel = "MSE", xscale = log10, yscale = log10)
+    ax_nll = Axis(fig[2, 1], xlabel = "Rank", ylabel = "Expected NLL", xscale = log10)
 
     alg2label = Dict("srkf" => "SRKF", "enkf" => "EnKF", "etkf" => "ETKF", "cakf" => "CAKF")
     alg2color = Dict("srkf" => :black, "enkf" => :blue, "etkf" => :green, "cakf" => :red)
@@ -63,22 +81,57 @@ with_theme(T) do
         color = alg2color["srkf"],
     )
 
-    for alg in ["enkf", "etkf", "cakf"]
+    for alg in ["enkf", "etkf"]
+        # errorbars!(
+        #     ax_mse,
+        #     metrics[alg].wall_time,
+        #     metrics[alg].mse_median,
+        #     metrics[alg].mse_25,
+        #     metrics[alg].mse_75;
+        #     color = alg2color[alg],
+        # )
+
         scatterlines!(
             ax_mse,
-            ranks,
-            [metrics[alg][rank].mse for rank in ranks],
+            metrics[alg].wall_time,
+            metrics[alg].mse_median,
             label = alg2label[alg],
             color = alg2color[alg],
         )
+
+        # errorbars!(
+        #     ax_nll,
+        #     metrics[alg].wall_time,
+        #     metrics[alg].expected_nll_median,
+        #     metrics[alg].expected_nll_25,
+        #     metrics[alg].expected_nll_75;
+        #     color = alg2color[alg],
+        # )
+
         scatterlines!(
             ax_nll,
-            ranks,
-            [metrics[alg][rank].expected_nll for rank in ranks],
+            metrics[alg].wall_time,
+            metrics[alg].expected_nll_median,
             label = alg2label[alg],
             color = alg2color[alg],
         )
     end
+
+    scatterlines!(
+        ax_mse,
+        metrics["cakf"].wall_time,
+        metrics["cakf"].mse,
+        label = alg2label["cakf"],
+        color = alg2color["cakf"],
+    )
+
+    scatterlines!(
+        ax_nll,
+        metrics["cakf"].wall_time,
+        metrics["cakf"].expected_nll,
+        label = alg2label["cakf"],
+        color = alg2color["cakf"],
+    )
 
     axislegend(ax_mse)
     axislegend(ax_nll)
