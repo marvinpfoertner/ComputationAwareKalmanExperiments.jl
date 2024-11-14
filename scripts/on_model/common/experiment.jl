e8b72f21d7a79952c76f542432d6cdb8b62e2c3d
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-11-14 16:40:39 +0100

    Add wall time as metric in experiment

--- scripts/on_model/common/experiment.jl
+++ scripts/on_model/common/experiment.jl
@@ -6,6 +6,8 @@ function results(config::Dict)
             rng = Random.seed!(config["seed"])
         end
 
+        tic = Base.time_ns()
+
         if algorithm == "srkf"
             uᶠs = Kalman.srkf(dgmp_aug, mmod, ys_train_aug)
         elseif algorithm == "enkf"
@@ -28,6 +30,8 @@ function results(config::Dict)
             uᶠs = cakf(dgmp, mmod, ys_train; rank = config["rank"], ts = ts)
         end
 
+        wall_time = (Base.time_ns() - tic) / 1e9
+
         mse = mean([
             ComputationAwareKalmanExperiments.mse(ustar, mean(uᶠ)) for
             (ustar, uᶠ) in zip(ustars, uᶠs)
@@ -36,7 +40,7 @@ function results(config::Dict)
             mean(ComputationAwareKalmanExperiments.gaussian_nll.(ustar, mean(uᶠ), var(uᶠ))) for (ustar, uᶠ) in zip(ustars, uᶠs)
         ])
 
-        return @strdict uᶠs mse expected_nll
+        return @strdict uᶠ mse expected_nll wall_time
     end
 
     @info config["algorithm"] results["mse"] results["expected_nll"]
