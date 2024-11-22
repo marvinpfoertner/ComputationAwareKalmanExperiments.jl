ecbe3771d6f0cfb6e21947ffdc839710d5ce6c4b
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2024-11-22 18:20:29 +0000

    Refactor ensemble filter loops

--- src/EnsembleKalman/filters.jl
+++ src/EnsembleKalman/filters.jl
@@ -1,105 +1,109 @@
-function enkf(
-    dgmp::ComputationAwareKalman.AbstractGaussMarkovChain,
-    mmod::ComputationAwareKalman.AbstractMeasurementModel,
-    ys;
-    rng::Random.AbstractRNG,
-    rank::Integer,
-)
-    u₀ = initialize_sample(dgmp, rng = rng, rank = rank)
+function filter(init_state::SquareRootGaussian, predict, update, ys)
+    uᶠs = SquareRootGaussian[]
 
-    states = typeof(u₀)[]
-    uₖ₋₁ = u₀
+    uᶠₖ₋₁ = init_state
 
     for (k, yₖ) in enumerate(ys)
-        u⁻ₖ = predict_sample_pointwise(
-            uₖ₋₁,
-            ComputationAwareKalman.A_b_lsqrt_Q(dgmp, k - 1)...,
-            rng,
-        )
+        u⁻ₖ = predict(uᶠₖ₋₁, k)
 
         if ismissing(yₖ)
-            uₖ = u⁻ₖ
+            uᶠₖ = u⁻ₖ
         else
-            uₖ = update_enkf(
-                u⁻ₖ,
-                yₖ,
-                ComputationAwareKalman.H(mmod, k),
-                ComputationAwareKalman.Λ(mmod, k),
-                rng,
-            )
+            uᶠₖ = update(u⁻ₖ, k, yₖ)
         end
 
-        push!(states, uₖ)
-        uₖ₋₁ = uₖ
+        push!(uᶠs, uᶠₖ)
+        uᶠₖ₋₁ = uᶠₖ
     end
 
-    return states
+    return uᶠs
+end
+
+function enkf(
+    gmc::ComputationAwareKalman.AbstractGaussMarkovChain,
+    mmod::ComputationAwareKalman.AbstractMeasurementModel,
+    ys;
+    rank::Integer,
+    rng::Random.AbstractRNG,
+)
+    u₀ = initialize_sample(gmc; rng = rng, rank = rank)
+
+    function predict(uᶠₖ₋₁, k)
+        return predict_sample_pointwise(
+            uᶠₖ₋₁,
+            ComputationAwareKalman.A_b_lsqrt_Q(gmc, k - 1)...,
+            rng,
+        )
+    end
+
+    function update(u⁻ₖ, k, yₖ)
+        return update_enkf(
+            u⁻ₖ,
+            yₖ,
+            ComputationAwareKalman.H(mmod, k),
+            ComputationAwareKalman.Λ(mmod, k),
+            rng,
+        )
+    end
+
+    return filter(u₀, predict, update, ys)
 end
 
 function etkf(
-    dgmp::ComputationAwareKalman.AbstractGaussMarkovChain,
+    gmc::ComputationAwareKalman.AbstractGaussMarkovChain,
+    mmod::ComputationAwareKalman.AbstractMeasurementModel,
+    ys;
+    rank::Integer,
+    rng::Random.AbstractRNG,
+    init_state::SquareRootGaussian = initialize_lanczos(gmc; rng = rng, rank = rank),
+    predict = (uᶠₖ₋₁, k) -> predict_lanczos(
+        uᶠₖ₋₁,
+        ComputationAwareKalmanExperiments.transition_model(gmc, k - 1)...;
+        rank = rank,
+        initvec = randn(rng, size(uᶠₖ₋₁.m, 1)),
+    ),
+)
+    function update(u⁻ₖ, k, yₖ)
+        return update_etkf(
+            u⁻ₖ,
+            yₖ,
+            ComputationAwareKalman.H(mmod, k),
+            ComputationAwareKalman.Λ(mmod, k),
+        )
+    end
+
+    return filter(init_state, predict, update, ys)
+end
+
+function etkf_truncate(
+    gmc::ComputationAwareKalman.AbstractGaussMarkovChain,
     mmod::ComputationAwareKalman.AbstractMeasurementModel,
     ys;
     rank::Integer,
     truncate_kwargs = (;),
+    init_state::SquareRootGaussian = initialize_truncate(
+        gmc;
+        rank = rank,
+        truncate_kwargs = truncate_kwargs,
+    ),
 )
-    u₀ = initialize_truncate(dgmp; rank = rank, truncate_kwargs = truncate_kwargs)
-
-    states = typeof(u₀)[]
-    uₖ₋₁ = u₀
-
-    for (k, yₖ) in enumerate(ys)
-        u⁻ₖ = predict_truncate(
-            uₖ₋₁,
-            ComputationAwareKalman.A_b_lsqrt_Q(dgmp, k - 1)...;
+    function predict(uᶠₖ₋₁, k)
+        return predict_truncate(
+            uᶠₖ₋₁,
+            ComputationAwareKalman.A_b_lsqrt_Q(gmc, k - 1)...;
             rank = rank,
             truncate_kwargs = truncate_kwargs,
         )
+    end
 
-        uₖ = update_etkf(
+    function update(u⁻ₖ, k, yₖ)
+        return update_etkf(
             u⁻ₖ,
             yₖ,
             ComputationAwareKalman.H(mmod, k),
             ComputationAwareKalman.Λ(mmod, k),
         )
-
-        push!(states, uₖ)
-        uₖ₋₁ = uₖ
     end
 
-    return states
-end
-
-function etkf_lanczos(
-    dgmp::ComputationAwareKalman.AbstractGaussMarkovChain,
-    mmod::ComputationAwareKalman.AbstractMeasurementModel,
-    ys;
-    rng::Random.AbstractRNG,
-    rank::Integer,
-)
-    u₀ = initialize_lanczos(dgmp; rng = rng, rank = rank)
-
-    states = typeof(u₀)[]
-    uₖ₋₁ = u₀
-
-    for (k, yₖ) in enumerate(ys)
-        u⁻ₖ = predict_lanczos(
-            uₖ₋₁,
-            ComputationAwareKalmanExperiments.transition_model(dgmp, k - 1)...;
-            rank = rank,
-            # initvec = randn(rng, size(uₖ₋₁.m, 1)),
-        )
-
-        uₖ = update_etkf(
-            u⁻ₖ,
-            yₖ,
-            ComputationAwareKalman.H(mmod, k),
-            ComputationAwareKalman.Λ(mmod, k),
-        )
-
-        push!(states, uₖ)
-        uₖ₋₁ = uₖ
-    end
-
-    return states
+    return filter(init_state, predict, update, ys)
 end
